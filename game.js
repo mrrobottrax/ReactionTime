e7c9afb3af6c428bbb8ee2596c1c240d993aecb4
Andrew <mrrobottrax@gmail.com>
2025-02-24 00:00:13 -0500

    Game

--- game.js
+++ game.js
@@ -8,13 +8,51 @@ function onLoad() {
 
 	box = document.getElementById("box");
 	button = document.getElementById("button");
+	statText = document.getElementById("status");
 
-	button.onclick = startGame;
+	resetGame();
 }
 
-function startGame()
-{
+function resetGame() {
+	statText.innerText = "Press start.";
+	button.innerText = "START";
+
+	button.onclick = startGame;
+
+	box.style.backgroundColor = "red";
+
+	if (typeof timeout !== 'undefined')
+		clearTimeout(timeout);
+}
+
+function startGame() {
+	statText.innerText = "Wait for the square to go green.";
 	button.innerText = "PRESS";
 
-	rand = Math.random() * maxTime + minTime;
+	button.onclick = tooEarly;
+
+	let wait = Math.random() * maxTime + minTime;
+
+	timeout = setTimeout(startTimer, wait * 1000);
+}
+
+function startTimer() {
+	box.style.backgroundColor = "green";
+
+	button.onclick = stopTimer;
+
+	timerStart = performance.now();
+}
+
+function stopTimer() {
+	resetGame();
+
+	let timeDelta = performance.now() - timerStart;
+	statText.innerText = "Time: " + timeDelta.toString() + "ms";
+}
+
+function tooEarly() {
+	resetGame();
+
+	statText.innerText = "Too early.";
 }
